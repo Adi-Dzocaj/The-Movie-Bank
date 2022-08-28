8953737397258beeb84059309ef25c7067dbd071
Adi Dzocaj <adidzocaj@gmail.com>
2022-08-28 20:19:47 +0200

    Having some issues with the _redirects folder not solving the error message that displays on refresh when deployed. Changing BrowserRouter to HashRouter and testing if this solves the problem.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import {
 	QueryClient,
@@ -14,11 +14,11 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 			<QueryClientProvider client={queryClient}>
-				<BrowserRouter>
+				<HashRouter>
 					<UrlContextProvider>
 						<App />
 					</UrlContextProvider>
-				</BrowserRouter>
+				</HashRouter>
 				<ReactQueryDevtools initialIsOpen={false} />
 			</QueryClientProvider>
 
