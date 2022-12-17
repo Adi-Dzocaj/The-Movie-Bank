d0a850f24f694673f1e36d6db50b2978868c636e
Adi <adidzocaj@gmail.com>
2022-12-17 16:08:55 +0100

    Made card images not blurry with object-fit: cover and some other style configurations

--- src/components/MovieCards.jsx
+++ src/components/MovieCards.jsx
@@ -12,9 +12,9 @@ const MovieCards = ({ data }) => {
 				<div style={{display: "flex", justifyContent: "center", flexWrap: "wrap", gap: "10px"}}>
 					{data.results.map((film, i) => {
 						return (
-						<Link style={{color: "white", textDecoration: "none"}} className='popularMovieCard' key={i} to={`/movie/${film.id}`}>
-							<Card style={{width: 200, height: 250, backgroundColor: "black"}}>
-								<Card.Img variant='top' style={{width: 200, height: 100}} src={UrlPrefixer.concat(film.poster_path)} alt=""/>
+						<Link style={{color: "white", textDecoration: "none", flex: '1 1 20%'}} className='popularMovieCard' key={i} to={`/movie/${film.id}`}>
+							<Card style={{height: '100%', backgroundColor: "black"}}>
+								<Card.Img variant='top' style={{ height: '70%', objectFit: 'cover'}} src={UrlPrefixer.concat(film.poster_path)} alt=""/>
 								<Card.Body>
 									<Card.Title style={{height: "70px", textAlign: "center"}}>{film.title}</Card.Title>
 									<Card.Footer>Score: {film.vote_average}</Card.Footer>
